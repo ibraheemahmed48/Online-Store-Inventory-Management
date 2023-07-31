01afcc02ed8946bbd67c443b682be38d422f1838
Ibraheem Ahmed <ibraheemahmed836@gmail.com>
2023-07-31 17:28:11 +0300

    finsh task

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import Vue from 'vue';
 import App from './App.vue';
 import router from './router/index';
-import store from './store';
 import firebaseConfig from './firebase/firebaseConfig';
 import { createApp } from 'vue';
 import firebase from 'firebase/app';
@@ -10,13 +8,7 @@ import 'firebase/firestore';
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-Vue.config.productionTip = false;
 
-new Vue({
-  
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
 
 
 
--- src/store/index.js
+++ src/store/index.js
@@ -61,3 +61,4 @@ export default new Vuex.Store({
     },
   },
 });
+
